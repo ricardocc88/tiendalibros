e8e600729f36455fa9b36a08923c82a6ac3ba3fc
RICARDO CARRASCO <ricardocc1988@gmail.com>
2024-06-28 12:35:53 -0400

    crear serviciocliente-funcion-listar-consola-log

--- admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
+++ admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
   selector: 'app-index-cliente',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexClienteComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _clienteService : ClienteService
+  ) { }
 
   ngOnInit(): void {
+    this._clienteService.listar_clientes_filtro_admin().subscribe(
+      response=>{
+        console.log(response);
+      },
+      error=>{
+        console.log(error)
+      }
+    )
   }
 
 }
