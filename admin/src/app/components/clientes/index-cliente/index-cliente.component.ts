f29a983559874083a52e0d67359edca2a7c1e806
RICARDO CARRASCO <ricardocc1988@gmail.com>
2024-07-15 21:44:37 -0400

    fin de token

--- admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
+++ admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { error } from 'console';
+import { AdminService } from 'src/app/services/admin.service';
 import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
@@ -9,77 +9,87 @@ import { ClienteService } from 'src/app/services/cliente.service';
 })
 export class IndexClienteComponent implements OnInit {
 
-  public clientes : Array<any>=[];
+  public clientes: Array<any> = [];
   public filtro_apellidos = '';
   public filtro_correo = '';
   public filtro_nombres = '';
 
-  public pageSize = 2;
   public page = 1;
+  public pageSize = 1;
+  public token;
 
   constructor(
-    private _clienteService : ClienteService
-  ) { }
+    private _clienteService: ClienteService,
+    private _adminService: AdminService
+  ) { 
+    this.token = this._adminService.getToken();
+    console.log(this.token);
+  }
 
   ngOnInit(): void {
     this.init_Data();
   }
-  init_Data(){
-    this._clienteService.listar_clientes_filtro_admin(null,null).subscribe(
-      response=>{
-        
-        this.clientes = response.data;
 
-        console.log(response);
+  init_Data(): void {
+    this._clienteService.listar_clientes_filtro_admin(null, null, this.token).subscribe(
+      response => {
+        this.clientes = response.data;
+        console.log(this.clientes);
       },
-      error=>{
-        console.log(error)
+      error => {
+        console.log(error);
       }
-    )
+    );
   }
 
-  filtro(tipo: any){
-
+  filtro(tipo: any): void {
     console.log(tipo);
-    console.log(this.filtro_nombres);
     console.log(this.filtro_apellidos);
     console.log(this.filtro_correo);
-    
+    console.log(this.filtro_nombres);
 
-    if(tipo == 'apellidos'){
-      this._clienteService.listar_clientes_filtro_admin(tipo,this.filtro_apellidos).subscribe(
-        response=>{
-          this.clientes = response.data;
-          console.log(this.clientes);
-
-        },
-        error=>{
-          console.log(error);
-        }
-      );
-    }else if(tipo== 'correo'){
-      this._clienteService.listar_clientes_filtro_admin(tipo,this.filtro_correo).subscribe(
-        response=>{
-          this.clientes = response.data;
-          console.log(this.clientes);
-        },error=>{
-          console.log(error);
-        }
-      );
-    }else if(tipo == 'nombres'){
-      this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_nombres).subscribe(
-        response=>{
-          this.clientes = response.data;
-          console.log(this.clientes);
-        },error =>{
-          console.log(error);
-        }
-      )
+    if (tipo == 'apellidos') {
+      if (this.filtro_apellidos) {
+        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_apellidos, this.token).subscribe(
+          response => {
+            this.clientes = response.data;
+            console.log(this.clientes);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      } else {
+        this.init_Data();
+      }
+    } else if (tipo == 'correo') {
+      if (this.filtro_correo) {
+        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_correo, this.token).subscribe(
+          response => {
+            this.clientes = response.data;
+            console.log(this.clientes);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      } else {
+        this.init_Data();
+      }
+    } else if (tipo == 'nombres') {
+      if (this.filtro_nombres) {
+        this._clienteService.listar_clientes_filtro_admin(tipo, this.filtro_nombres, this.token).subscribe(
+          response => {
+            this.clientes = response.data;
+            console.log(this.clientes);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      } else {
+        this.init_Data();
+      }
     }
-    else{
-      this.init_Data();
-    }
-
   }
-
 }
--- admin/src/app/services/cliente.service.ts
+++ admin/src/app/services/cliente.service.ts
@@ -16,8 +16,8 @@ export class ClienteService {
 
       this.url = GLOBAL.url;
    }
-   listar_clientes_filtro_admin(tipo: string | null,filtro: string | null):Observable<any>{
-    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+   listar_clientes_filtro_admin(tipo: string | null,filtro: string | null,token:any):Observable<any>{
+    let headers = new HttpHeaders({'Content-Type':'application/json', 'Authorization':token});
     return this._http.get(this.url+'listar_clientes_filtro_admin/'+tipo+'/'+filtro,{headers:headers});
    }
 }
